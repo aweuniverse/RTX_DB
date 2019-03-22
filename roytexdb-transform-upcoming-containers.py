1484ad8e256acd6ccff12e5378f698ccc9b753df
aweuniverse <pingbu2011@gmail.com>
2019-03-22 16:07:03 -0400

    Add files via upload

--- roytexdb-transform-upcoming-containers.py
+++ roytexdb-transform-upcoming-containers.py
@@ -4,12 +4,6 @@ Created on Fri Feb  1 10:10:59 2019
 
 @author: PBu
 
-log 3/19/2019
-need data input control:
-    1), contorl container# fits a certain format
-    2), control HFC# is valid (FK references HFC_HEADER table)
-    3), control that sum (CARTON_CTN) by container in HFC_CONTAINER table matches TTL_CARTON in CONTAINER table
-
 """
 
 import pandas as pd
@@ -18,13 +12,16 @@ import pyodbc
 import sqlalchemy
 import logging
 import sys
+import re
+from datetime import datetime
 
 
 """
 from excel source create a dataframe that maps to HFC_CONTAINER table
+data quality control#1: no null HFC
 """
 pd_container = pd.read_excel('W:\\Roytex - The Method\\Ping\\ROYTEXDB\\UPCOMING_CONTAINERS.xlsx', skiprows=3, header=None, 
-                             usecols=[3,4,5],names=['CONTAINER_NBR', 'HFC_NBR', 'CARTON_CTN'], converters={0: np.str, 1: np.str, 2: np.int32})
+                             usecols=[2,3,4,5],names=['ETA', 'CONTAINER_NBR', 'HFC_NBR', 'CARTON_CTN'], converters={1: np.str, 2: np.str, 3: np.int32})
 pd_container = pd_container.iloc[:-1]
 
 if sum(pd_container['HFC_NBR'].isnull()) > 0:
@@ -33,20 +30,14 @@ if sum(pd_container['HFC_NBR'].isnull()) > 0:
 
 pd_container['HFC_NBR'] = pd_container['HFC_NBR'].apply(lambda x:x.zfill(6))
 pd_container['CONTAINER_NBR'] = pd_container['CONTAINER_NBR'].fillna(method='ffill')
+pd_container['ETA'] = pd_container['ETA'].fillna(method='ffill')
+pd_container['ETA'] = pd_container['ETA'].dt.date
 pd_container.reset_index(drop=True, inplace=True)
 
-"""
-from the same excel source create a dataframe that maps to CONTAINER table
-"""
-pd_container_ttl = pd.read_excel('W:\\Roytex - The Method\\Ping\\ROYTEXDB\\UPCOMING_CONTAINERS.xlsx', skiprows=3, header=None, 
-                             usecols=[2,3,6],names=['ETA', 'CONTAINER_NBR', 'TTL_CTN'])
-pd_container_ttl = pd_container_ttl.iloc[:-1]
-pd_container_ttl.dropna(inplace=True)
-pd_container_ttl['ETA'] = pd_container_ttl['ETA'].dt.date
-pd_container_ttl.reset_index(drop=True, inplace=True)
 
 """
-data input control #1 - read all valid HFC_NBR from SQL and make sure pd_container has one of the valid HFC numbers
+data quality control#2 - read all valid HFC_NBR from SQL and make sure pd_container has one of the valid HFC numbers
+***this requires that all the HFCs be loaded in SQL HFC_HEADER table first
 """
 engine = sqlalchemy.create_engine("mssql+pyodbc://@sqlDSN")
 pd_valid_hfc = pd.read_sql_table('HFC_HEADER', con=engine, columns = ['HFC_NBR'])
@@ -58,39 +49,75 @@ if sum(~pd_container['HFC_NBR'].isin(pd_valid_hfc['HFC_NBR'])) > 0:
         print ('WARNING: ' + pd_container['HFC_NBR'].iloc[exception_index[n]] + ' in the container file is not a valid HFC')
     sys.exit('FIX ABOVE MENTIONED ERROR(S)')
 
-#pd_container.to_sql(name='temp_hfc_container', con=engine, if_exists='replace', index=False)
-#
-#
-#conn = pyodbc.connect('Driver={SQL Server};'
-#                  'Server=DESKTOP-5JROCDL\SQLEXPRESS;'
-#                  'Database=roytexdb;'
-#                  'Trusted_Connection=yes;')
-#c = conn.cursor()  
-#
-##c.execute('''
-##         create table #temp_hfc_container
-##         (ETA date not null, CONTAINER_NBR varchar(15) not null, HFC_NBR varchar(6) not null, CARTON_CTN int not null)
-##         ''')
-#
-#try:
-#    c.execute('''
-#              MERGE DBO.HFC_CONTAINER AS T
-#              USING temp_hfc_container AS S
-#              ON (T.HFC_NBR = S.HFC_NBR and T.CONTAINER_NBR = S.CONTAINER_NBR)
-#              WHEN MATCHED THEN
-#              UPDATE SET T.ETA = S.ETA, T.CARTON_CTN=S.CARTON_CTN
-#              WHEN NOT MATCHED BY TARGET THEN
-#              INSERT (HFC_NBR, CONTAINER_NBR, ETA, CARTON_CTN) VALUES (S.HFC_NBR, S.CONTAINER_NBR, S.ETA, S.CARTON_CTN);
-#              ''')
-#    c.execute('''drop table temp_hfc_container;''')
-#    conn.commit()
-#    conn.close()
-#except Exception as e:
-##    c.execute('''drop table temp_hfc_container;''')
-##    conn.commit()
-#    logger = logging.Logger('Catch_All')
-#    logger.error(str(e))
-#    conn.close()
-#
-#
-#
+
+"""
+from the same excel source create a dataframe that gets total carton count by container
+data quality control#3: container number fits format ^[A-Z]{4}[0-9]{7}$ if it's not AIR
+"""
+pd_container_ttl = pd.read_excel('W:\\Roytex - The Method\\Ping\\ROYTEXDB\\UPCOMING_CONTAINERS.xlsx', skiprows=3, header=None, 
+                             usecols=[2,3,6],names=['ETA', 'CONTAINER_NBR', 'TTL_CTN'])
+pd_container_ttl = pd_container_ttl.iloc[:-1]
+pd_container_ttl.dropna(inplace=True)
+pd_container_ttl['ETA'] = pd_container_ttl['ETA'].dt.date
+pd_container_ttl.reset_index(drop=True, inplace=True)
+
+pattern = re.compile('^[A-Z]{4}[0-9]{7}$')
+for n in range(pd_container_ttl.shape[0]):
+    if pd_container_ttl['CONTAINER_NBR'][n] != 'AIR':
+        if not pattern.match(pd_container_ttl['CONTAINER_NBR'][n]):
+            print ('WARNING: CONTAINER NBR '+ pd_container_ttl['CONTAINER_NBR'][n] + ' is invalid!')
+            sys.exit('FIX ABOVE MENTIONED ERROR(S)')
+
+#pd_container_ttl = pd.pivot_table(pd_container_ttl, index=['CONTAINER_NBR'], aggfunc=np.sum).reset_index()
+
+"""
+Once all data quality checks passed, proceed to update SQL HFC_CONTAINER table
+Quality control#4: 
+Once HFC_CONTAINER table updated, read updated total carton count by container and compare it to the original pd_container_ttl table
+if different, raise error 
+"""
+pd_container.to_sql(name='temp_hfc_container', con=engine, if_exists='replace', index=False)
+pd_container_ttl.to_sql(name='temp_container_ttl', con=engine, if_exists='replace', index=False)
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                  'Server=DESKTOP-5JROCDL\SQLEXPRESS;'
+                  'Database=roytexdb;'
+                  'Trusted_Connection=yes;')
+c = conn.cursor()  
+
+try:
+    c.execute('''
+              MERGE DBO.HFC_CONTAINER AS T
+              USING temp_hfc_container AS S
+              ON (T.HFC_NBR = S.HFC_NBR and T.CONTAINER_NBR = S.CONTAINER_NBR)
+              WHEN MATCHED THEN
+              UPDATE SET T.CARTON_CTN=S.CARTON_CTN, T.ETA=S.ETA
+              WHEN NOT MATCHED BY TARGET THEN
+              INSERT (HFC_NBR, CONTAINER_NBR, CARTON_CTN, ETA) VALUES (S.HFC_NBR, S.CONTAINER_NBR, S.CARTON_CTN, S.ETA);
+              ''')
+    c.execute('''drop table temp_hfc_container;''')
+    after_update = '''
+                   SELECT CONTAINER_NBR, ETA, SUM (CARTON_CTN) AS TTL_CARTON 
+                   FROM DBO.HFC_CONTAINER WHERE CONTAINER_NBR IN (SELECT DISTINCT CONTAINER_NBR FROM dbo.temp_container_ttl)
+                   GROUP BY CONTAINER_NBR, ETA
+                   '''
+    pd_container_ttl_after = pd.read_sql(after_update, conn)
+    c.execute('''drop table temp_container_ttl;''')
+    conn.commit()
+    conn.close()
+except Exception as e:
+    logger = logging.Logger('Catch_All')
+    logger.error(str(e))
+    conn.close()
+
+pd_container_ttl_after['ETA'] = pd_container_ttl_after['ETA'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').date())
+pd_validate = pd.merge(pd_container_ttl, pd_container_ttl_after, how='left', 
+                       left_on=['CONTAINER_NBR', 'ETA'], right_on=['CONTAINER_NBR', 'ETA'])
+for n in range(pd_validate.shape[0]):
+    if pd_validate['TTL_CTN'][n] - pd_validate['TTL_CARTON'][n] != 0:
+        print ('WARNING: Container ' + pd_validate['CONTAINER_NBR'][n] + ' with ETA ' + pd_validate['ETA'][n].strftime('%Y-%m-%d') + ' has error!')
+        sys.exit('FIX ABOVE MENTIONED ERROR(S)')
+
+print ('UPDATE COMPLETED SUCCESSFULLY!')
+
+
