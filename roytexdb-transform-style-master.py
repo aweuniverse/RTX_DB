665fcbd74890cc408a1784fa3235bb3df506b80e
aweuniverse <pingbu2011@gmail.com>
2019-09-19 16:34:25 -0400

    added CAT, INV_GROUP, BUY_GROUP_MAIN

--- roytexdb-transform-style-master.py
+++ roytexdb-transform-style-master.py
@@ -7,7 +7,7 @@ STATEMENT OF PURPOSE:
 PREREQUISITE:
     SQL table SIZE_RANGE has to be loaded before running this program
 """
-
+import sys
 import pandas as pd
 import numpy as np
 import sqlalchemy
@@ -21,17 +21,34 @@ df_style = pd.read_csv('W:\Roytex - The Method\Ping\ROYTEXDB\SOURCE_STYLE_MASTER
 df_style['STYLE'] = df_style['STYLE'].apply(lambda x: x.zfill(6) if len(x) < 6 else x)
 df_style['SEASON'] = df_style.apply(lambda row: 'SP-' + row.OPEN_YR[-2:] if row.OPEN_SSN.upper() == 'S' else 'FA-' + row.OPEN_YR[-2:], axis=1)
 #df_style['OPEN_SSN'] = df_style['OPEN_SSN'].apply(lambda x: x.upper())
-#df_style['CAT'] = df_style['STYLE_DESC'].apply(lambda x: '' if type(x)==np.float else 'K' if ' KS ' in x or ' KL ' in x else ('W' if ' WS ' in x or ' WL ' in x else ''))
+df_style['CAT'] = df_style['STYLE_DESC'].apply(lambda x: '' if type(x)==np.float else 'K' if ' KS ' in x or ' KL ' in x else ('W' if ' WS ' in x or ' WL ' in x else ''))
 df_style['SLV'] = df_style['STYLE_DESC'].apply(lambda x: '' if type(x)==np.float else 'L/S' if ' KL ' in x or ' WL ' in x else ('S/S' if ' WS ' in x or ' KS ' in x else ''))
 
 slvAndSize = pd.read_csv('W:\Roytex - The Method\Ping\ROYTEXDB\SOURCE_STYLE_MASTER_SLV_AND_SIZE.csv', converters={0:str})
 slvAndSize['STYLE'] = slvAndSize['STYLE'].apply(lambda x: x.zfill(6) if len(x) < 6 else x)
 
+styleNew = pd.merge(df_style, slvAndSize, how='left', on='STYLE')
+styleNew.fillna({'CAT_2': ''}, inplace=True)
+styleNew.fillna({'SLV_2': ''}, inplace=True)
+# CHECKING DISCREPANCY BETWEEN PROCOMM DESCRIPTION AND THE LIST I MANUALLY MAINTAIN IS A GOOD WAY TO MAKE SURE MY LIST IS CORRECT
+exceptionCAT = styleNew[(styleNew['CAT'] != '') & (styleNew['CAT_2'] != '') & (styleNew['CAT'] != styleNew['CAT_2'])]
+exceptionCAT['TYPE'] = 'CAT'
+exceptionSLV = styleNew[(styleNew['SLV'] != '') & (styleNew['SLV_2'] != '') & (styleNew['SLV'] != styleNew['SLV_2'])]
+exceptionSLV['TYPE'] = 'SLV'
+exception = pd.concat([exceptionCAT, exceptionSLV], ignore_index=True)
+if exception.shape[0] > 0:
+    exception.to_excel('ExceptionReport_STYLE_MASTER.xlsx', index=False)
+    choice = str(input('STYLE MASTER EXCEPTION REPORT OFFLOADED. PLEASE REVIEW. IS IT OKAY TO PROCEED? Y OR N')) #there should not be any style from 2019 & forward
+    if choice.upper() != 'Y':
+        sys.exit('PROGRAM STOPPED')
+# for the CAT & SLV, use the list I manually maintained as the final answer
+styleNew['CAT'] = styleNew.apply(lambda row: row.CAT if row.CAT_2 == '' else row.CAT_2, axis=1)
+styleNew['SLV'] = styleNew.apply(lambda row: row.SLV if row.SLV_2 == '' else row.SLV_2, axis=1)
+
 engine = sqlalchemy.create_engine("mssql+pyodbc://@sqlDSN")
 conn = engine.connect()
-df_style.to_sql(name='#temp_style', con=conn, if_exists='replace', index=False)
-slvAndSize.to_sql(name='#temp_slv_and_size', con=conn, if_exists='replace', index=False)
-                  
+styleNew.to_sql(name='#temp_style', con=conn, if_exists='replace', index=False)
+#slvAndSize.to_sql(name='#temp_slv_and_size', con=conn, if_exists='replace', index=False)
 trans = conn.begin()
 try:
     conn.execute("""MERGE DBO.STYLE_MASTER AS T 
@@ -39,15 +56,15 @@ try:
                  ON T.STYLE = S.STYLE
                  WHEN MATCHED THEN UPDATE 
                  SET T.STYLE_DESC = S.STYLE_DESC, T.DIV=S.DIV, T.PROTO=S.PROTO, T.SIZE_RANGE_CODE = S.SIZE_RANGE, T.SLV = S.SLV,
-                 T.SEASON = S.SEASON, T.LABEL_CODE=S.LABEL, 
+                 T.SEASON = S.SEASON, T.LABEL_CODE=S.LABEL, T.CAT = S.CAT, T.INV_GROUP = S.INV_GROUP, T.BUY_GROUP_MAIN = S.BUY_GROUP_MAIN,
                  T.SP=S.SP, T.ELC=S.ELC, T.ILC=S.ILC, T.BRAND_NAME=S.BRAND_NAME, 
                  T.FABRIC=S.FABRIC, T.ROYALTY = S.ROYALTY, T.PPK = S.PPK, T.CXL=0
                  WHEN NOT MATCHED BY TARGET THEN 
-                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, ROYALTY, PPK) 
-                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.ROYALTY, S.PPK)
+                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, ROYALTY, PPK, CAT, INV_GROUP, BUY_GROUP_MAIN) 
+                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.ROYALTY, S.PPK, S.CAT, S.INV_GROUP, S.BUY_GROUP_MAIN)
                  WHEN NOT MATCHED BY SOURCE THEN
                  UPDATE SET T.CXL = 1;""")
-    conn.execute("""update T set T.REG_BT = S.SIZE, T.SLV = S.SLV from #temp_slv_and_size as S inner join dbo.STYLE_MASTER as T on T.STYLE=S.STYLE""")
+#    conn.execute("""update T set T.REG_BT = S.SIZE, T.SLV = S.SLV from #temp_slv_and_size as S inner join dbo.STYLE_MASTER as T on T.STYLE=S.STYLE""")
     trans.commit()
 #    conn.close()
 #    engine.dispose()
@@ -58,6 +75,7 @@ except Exception as e:
     trans.rollback()
     conn.close()
     engine.dispose()
+    sys.exit('PROGRAM STOPPED')
 
 """
 Below code is added for data integrity check:
@@ -70,15 +88,14 @@ sql_check_ssn_code = '''
                     ON F.STYLE = S.STYLE
                     '''
 check_ssn_code = pd.read_sql(sql_check_ssn_code, con = conn)
+conn.close()
+engine.dispose()
 ssn_code_exception = check_ssn_code[check_ssn_code['HFC_SSN'] != check_ssn_code['STYLE_SSN']]
 if ssn_code_exception.shape[0] > 0:
     print ('Some styles might have season code issue. Please review below data')
     print (ssn_code_exception.reset_index(drop=True))
 else:
     print ('All styles passed season code check')
-
-conn.close()
-engine.dispose()
 
 
 #import csv
