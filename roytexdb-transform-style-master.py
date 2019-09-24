d7ca84e20ca409692a177c4b82f11f70c2a89982
aweuniverse <pingbu2011@gmail.com>
2019-09-24 12:59:13 -0400

    Added auto-determined RM_GROUP

--- roytexdb-transform-style-master.py
+++ roytexdb-transform-style-master.py
@@ -44,6 +44,18 @@ if exception.shape[0] > 0:
 # for the CAT & SLV, use the list I manually maintained as the final answer
 styleNew['CAT'] = styleNew.apply(lambda row: row.CAT if row.CAT_2 == '' else row.CAT_2, axis=1)
 styleNew['SLV'] = styleNew.apply(lambda row: row.SLV if row.SLV_2 == '' else row.SLV_2, axis=1)
+styleNew['INV_GROUP'].fillna('', inplace=True)
+styleNew['BUY_GROUP_MAIN'].fillna('', inplace=True)
+styleNew['RM_GROUP'] = styleNew.apply(lambda row: "POLY KNITS" if ((row.INV_GROUP == 'POLY POLO') | (row.INV_GROUP == 'POLY QTR ZIP') | (row.INV_GROUP == 'PRINT POLY (KPR GROUP)'))
+                                                else "MICROFIBER" if ((row.INV_GROUP == 'MICROFIBER') | (row.INV_GROUP == 'MICROFIBER PRINT'))
+                                                else "OTHER KNITS" if (row.CAT == 'K')
+                                                else "OTHER WOVEN" if (row.CAT == 'W') else '', axis = 1)
+#
+#slvAndSize2 = pd.read_csv('W:\Roytex - The Method\Ping\ROYTEXDB\SOURCE_STYLE_MASTER_SLV_AND_SIZE_2.csv', converters={0:str})
+#slvAndSize2['STYLE'] = slvAndSize2['STYLE'].apply(lambda x: x.zfill(6) if len(x) < 6 else x)
+#engine = sqlalchemy.create_engine("mssql+pyodbc://@sqlDSN")
+#conn = engine.connect()
+#slvAndSize2.to_sql(name='#temp_slv_and_size', con=conn, if_exists='replace', index=False)
 
 engine = sqlalchemy.create_engine("mssql+pyodbc://@sqlDSN")
 conn = engine.connect()
@@ -56,15 +68,15 @@ try:
                  ON T.STYLE = S.STYLE
                  WHEN MATCHED THEN UPDATE 
                  SET T.STYLE_DESC = S.STYLE_DESC, T.DIV=S.DIV, T.PROTO=S.PROTO, T.SIZE_RANGE_CODE = S.SIZE_RANGE, T.SLV = S.SLV,
-                 T.SEASON = S.SEASON, T.LABEL_CODE=S.LABEL, T.CAT = S.CAT, T.INV_GROUP = S.INV_GROUP, T.BUY_GROUP_MAIN = S.BUY_GROUP_MAIN,
+                 T.SEASON = S.SEASON, T.LABEL_CODE=S.LABEL, T.CAT = S.CAT, T.INV_GROUP = S.INV_GROUP, T.BUY_GROUP_MAIN = S.BUY_GROUP_MAIN, T.RM_GROUP = S.RM_GROUP,
                  T.SP=S.SP, T.ELC=S.ELC, T.ILC=S.ILC, T.BRAND_NAME=S.BRAND_NAME, 
                  T.FABRIC=S.FABRIC, T.ROYALTY = S.ROYALTY, T.PPK = S.PPK, T.CXL=0
                  WHEN NOT MATCHED BY TARGET THEN 
-                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, ROYALTY, PPK, CAT, INV_GROUP, BUY_GROUP_MAIN) 
-                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.ROYALTY, S.PPK, S.CAT, S.INV_GROUP, S.BUY_GROUP_MAIN)
+                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, ROYALTY, PPK, CAT, INV_GROUP, BUY_GROUP_MAIN, RM_GROUP) 
+                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.ROYALTY, S.PPK, S.CAT, S.INV_GROUP, S.BUY_GROUP_MAIN, S.RM_GROUP)
                  WHEN NOT MATCHED BY SOURCE THEN
                  UPDATE SET T.CXL = 1;""")
-#    conn.execute("""update T set T.REG_BT = S.SIZE, T.SLV = S.SLV from #temp_slv_and_size as S inner join dbo.STYLE_MASTER as T on T.STYLE=S.STYLE""")
+#    conn.execute("""update T set T.REG_BT = S.SIZE, T.SLV = S.SLV_2, T.CAT=S.CAT_2, T.INV_GROUP=S.INV_GROUP from #temp_slv_and_size as S inner join dbo.STYLE_MASTER as T on T.STYLE=S.STYLE""")
     trans.commit()
 #    conn.close()
 #    engine.dispose()
