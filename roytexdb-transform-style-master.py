c09e2b5986ae53b2359d674205f50be8a054820f
aweuniverse <pingbu2011@gmail.com>
2019-10-03 15:38:07 -0400

    Added RESERVE field

--- roytexdb-transform-style-master.py
+++ roytexdb-transform-style-master.py
@@ -14,9 +14,9 @@ import sqlalchemy
 import logging
 
 df_style = pd.read_csv('W:\Roytex - The Method\Ping\ROYTEXDB\SOURCE_STYLE_MASTER.csv', header=None,
-                            names=['STYLE', 'STYLE_DESC', 'PROTO', 'SIZE_RANGE', 'OPEN_SSN', 'OPEN_YR', 'DIV', 'LABEL', 'SP', 'ELC', 'ILC', 'BRAND_NAME', 'FABRIC', 'ROYALTY', 'PPK' ],
-                            usecols=[1, 2, 3, 4, 5, 6, 7,8, 11, 12, 13, 15, 16, 30, 34], skiprows=2, 
-                            dtype={'STYLE': str, 'STYLE_DESC':str, 'PROTO':str, 'SIZE_RANGE':str, 'OPEN_SSN':str, 'OPEN_YR':str, 'DIV': np.int, 'LABEL':str, 'SP':np.float64, 'ELC':np.float64, 'ILC': np.float64, 'BRAND_NAME':str, 'FABRIC':str, 'ROYALTY':np.float64, 'PPK':str })
+                            names=['STYLE', 'STYLE_DESC', 'PROTO', 'SIZE_RANGE', 'OPEN_SSN', 'OPEN_YR', 'DIV', 'LABEL', 'SP', 'ELC', 'ILC', 'BRAND_NAME', 'FABRIC', 'RESERVE', 'ROYALTY', 'PPK' ],
+                            usecols=[1, 2, 3, 4, 5, 6, 7,8, 11, 12, 13, 15, 16, 29, 30, 34], skiprows=2, 
+                            dtype={'STYLE': str, 'STYLE_DESC':str, 'PROTO':str, 'SIZE_RANGE':str, 'OPEN_SSN':str, 'OPEN_YR':str, 'DIV': np.int, 'LABEL':str, 'SP':np.float64, 'ELC':np.float64, 'ILC': np.float64, 'BRAND_NAME':str, 'FABRIC':str, 'RESERVE': np.float64, 'ROYALTY':np.float64, 'PPK':str })
 
 df_style['STYLE'] = df_style['STYLE'].apply(lambda x: x.zfill(6) if len(x) < 6 else x)
 df_style['SEASON'] = df_style.apply(lambda row: 'SP-' + row.OPEN_YR[-2:] if row.OPEN_SSN.upper() == 'S' else 'FA-' + row.OPEN_YR[-2:], axis=1)
@@ -71,10 +71,10 @@ try:
                  SET T.STYLE_DESC = S.STYLE_DESC, T.DIV=S.DIV, T.PROTO=S.PROTO, T.SIZE_RANGE_CODE = S.SIZE_RANGE, T.SLV = S.SLV,
                  T.SEASON = S.SEASON, T.LABEL_CODE=S.LABEL, T.CAT = S.CAT, T.INV_GROUP = S.INV_GROUP, T.BUY_GROUP_MAIN = S.BUY_GROUP_MAIN, T.BUY_GROUP_SUB = S.BUY_GROUP_SUB, T.RM_GROUP = S.RM_GROUP,
                  T.SP=S.SP, T.ELC=S.ELC, T.ILC=S.ILC, T.BRAND_NAME=S.BRAND_NAME, 
-                 T.FABRIC=S.FABRIC, T.ROYALTY = S.ROYALTY, T.PPK = S.PPK, T.CXL=0
+                 T.FABRIC=S.FABRIC, T.RESERVE=S.RESERVE, T.ROYALTY = S.ROYALTY, T.PPK = S.PPK, T.CXL=0
                  WHEN NOT MATCHED BY TARGET THEN 
-                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, ROYALTY, PPK, CAT, INV_GROUP, BUY_GROUP_MAIN, BUY_GROUP_SUB, RM_GROUP) 
-                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.ROYALTY, S.PPK, S.CAT, S.INV_GROUP, S.BUY_GROUP_MAIN, S.BUY_GROUP_SUB, S.RM_GROUP)
+                 INSERT (STYLE, STYLE_DESC, DIV, PROTO, SIZE_RANGE_CODE, SEASON, ILC, ELC, SP, SLV, LABEL_CODE, BRAND_NAME, FABRIC, RESERVE, ROYALTY, PPK, CAT, INV_GROUP, BUY_GROUP_MAIN, BUY_GROUP_SUB, RM_GROUP) 
+                 VALUES (S.STYLE, S.STYLE_DESC, S.DIV, S.PROTO, S.SIZE_RANGE, S.SEASON, S.ILC, S.ELC, S.SP, S.SLV, S.LABEL, S.BRAND_NAME, S.FABRIC, S.RESERVE, S.ROYALTY, S.PPK, S.CAT, S.INV_GROUP, S.BUY_GROUP_MAIN, S.BUY_GROUP_SUB, S.RM_GROUP)
                  WHEN NOT MATCHED BY SOURCE THEN
                  UPDATE SET T.CXL = 1;""")
 #    conn.execute("""update T set T.REG_BT = S.SIZE, T.SLV = S.SLV_2, T.CAT=S.CAT_2, T.INV_GROUP=S.INV_GROUP from #temp_slv_and_size as S inner join dbo.STYLE_MASTER as T on T.STYLE=S.STYLE""")
